c510535563fb18efe2765e6954d3931b8469fe2f
Michael Curry <michaelcurry95@gmail.com>
2021-08-05 14:58:23 -0400

    tests

--- src/tests/index.ts
+++ src/tests/index.ts
@@ -1,9 +1,11 @@
+import { readFileSync } from 'fs';
+import path from 'path';
 import { expect } from 'chai';
-import { allowedToEmail, Cache } from '../index';
+import { allowedToEmail, ipInBlockList } from '../utils';
 
 describe('allowedToEmail should validate against cache', () => {
 	it('should return true if email isnt present in cache', () => {
-		let cache: Cache = {};
+		let cache: MyCache = {};
 
 		let email = 'notincache@email.com';
 
@@ -11,7 +13,7 @@ describe('allowedToEmail should validate against cache', () => {
 	});
 
 	it('should return false if email is present in cache within timeframe', () => {
-		let cache: Cache = {};
+		let cache: MyCache = {};
 
 		let email = 'incache@email.com';
 
@@ -21,12 +23,21 @@ describe('allowedToEmail should validate against cache', () => {
 	});
 
 	it('should return true if email is in cache, but outside of timeframe', () => {
-		let cache: Cache = {};
+		let cache: MyCache = {};
 
 		let email = 'totally.valid@email.com';
 
 		cache[email] = new Date('1995-06-28').getTime();
 
 		expect(allowedToEmail(email, cache)).to.be.true;
+	});
+});
+
+describe('ipInBlockList should return true if `req.ip` exists in blockList', () => {
+	it('should return true if email is in blockList', () => {
+		const blockList: BlockList = readFileSync(path.join(__dirname, './blocklist.text.txt'), 'utf-8').split('\n');
+		const ip: BlockedIP = blockList[0];
+
+		expect(ipInBlockList(ip, blockList)).to.be.true;
 	});
 });
