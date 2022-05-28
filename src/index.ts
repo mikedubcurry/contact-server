2cb37ca568a0e631aad479fe41a4485a6220c596
mikedubcurry <michaelcurry95@gmail.com>
2022-05-28 17:56:03 -0400

    new regex to check if message contains url

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
-import express from 'express';
-import morgan from 'morgan';
-import { createWriteStream, readFileSync } from 'fs';
-import path from 'path';
-import nodemailer from 'nodemailer';
-import dotenv from 'dotenv';
+import express from "express";
+import morgan from "morgan";
+import { createWriteStream, readFileSync } from "fs";
+import path from "path";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
 
-import { addToBlockList, allowedToEmail, loadBlockList, rejectInBlockList } from './utils';
+import {
+  addToBlockList,
+  allowedToEmail,
+  EmailFailedError,
+  loadBlockList,
+  rejectInBlockList,
+} from "./utils";
 
 dotenv.config();
 
@@ -14,92 +20,124 @@ let blockList: BlockList = loadBlockList();
 let cache: MyCache = {};
 
 const transport = nodemailer.createTransport({
-	service: 'gmail',
-	auth: {
-		user: process.env.EMAIL_ACCOUNT,
-		pass: process.env.EMAIL_PASS,
-	},
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_ACCOUNT,
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
 const app = express();
 
 app.use(
-	morgan('[:date[clf]] :remote-addr :method :url :status', {
-		stream: createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' }),
-		// log all requests except 404s
-		// skip: function (req, res) {
-		// 	return res.statusCode === 404;
-		// },
-	})
+  morgan("[:date[clf]] :remote-addr :method :url :status", {
+    stream: createWriteStream(path.join(__dirname, "access.log"), {
+      flags: "a",
+    }),
+    // log all requests except 404s
+    // skip: function (req, res) {
+    // 	return res.statusCode === 404;
+    // },
+  })
 );
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-	res.json({ status: 'success', message: 'good to go' });
+app.get("/", (req, res) => {
+  res.json({ status: "success", message: "good to go" });
 });
 
-app.get('/clearCache', (req, res) => {
-	cache = {};
-	res.json({ status: 'success', message: 'cache cleared' });
+app.get("/clearCache", (req, res) => {
+  cache = {};
+  res.json({ status: "success", message: "cache cleared" });
 });
 
 app.post(
-	'/sendEmail',
-	(req, res, next) => {
-		rejectInBlockList(req, res, next, blockList);
-	},
-	async (req, res) => {
-		const { name, email, message }: { name: string; email: string; message: string } = req.body;
-		if (!name || !email || !message) {
-			return res.status(400).json({ status: 'error', message: 'must send name, email and message' });
-		}
-		if (!allowedToEmail(email, cache, blockList)) {
-			return res.status(429).json({
-				status: 'error',
-				message: `user ${name}-${email} has already sent a message. try again in an hour`,
-			});
-		}
+  "/sendEmail",
+  (req, res, next) => {
+    rejectInBlockList(req, res, next, blockList);
+  },
+  async (req, res) => {
+    const {
+      name,
+      email,
+      message,
+    }: { name: string; email: string; message: string } = req.body;
+    if (!name || !email || !message) {
+      return res.status(400).json({
+        status: "error",
+        message: "must send name, email and message",
+      });
+    }
+    if (!allowedToEmail(email, cache, blockList)) {
+      return res.status(429).json({
+        status: "error",
+        message: `user ${name}-${email} has already sent a message. try again in an hour`,
+      });
+    }
 
-		if (message.length > 240) {
-			return res.status(400).json({ status: 'error', message: 'message exceeds allowed length' });
-		}
+    if (message.length > 240) {
+      return res
+        .status(400)
+        .json({ status: "error", message: "message exceeds allowed length" });
+    }
 
-		try {
-			const messageSent = (await transport.sendMail({
-				to: process.env.EMAIL_DESTINATION,
-				from: process.env.EMAIL_ACCOUNT,
-				subject: `Portfolio message from ${name}-${email}`,
-				html: new Date() + message,
-			})) as unknown as nodemailerResponse;
+    if (
+      new RegExp(
+        "([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?"
+      ).test(message)
+    ) {
+      return res
+        .status(400)
+        .json({ status: "error", message: "Do not send URLs" });
+    }
 
-			if (messageSent.rejected.length) {
-				return res.status(500).json({ status: 'error', message: 'message not sent' });
-			}
-			if (messageSent.response.includes(' OK ')) {
-				/* this is probably good */
+    try {
+      const messageSent = (await transport.sendMail({
+        to: process.env.EMAIL_DESTINATION,
+        from: process.env.EMAIL_ACCOUNT,
+        subject: `Portfolio message from ${name}-${email}`,
+        html: new Date() + message,
+      })) as unknown as nodemailerResponse;
 
-				cache[email] = new Date().getTime();
-				return res.status(200).json({ status: 'success', message: 'message sent' });
-			}
-		} catch (e) {
-			return res.status(500).json({ status: 'error', message: 'error in POST /sendMail' });
-		}
-	}
+      if (messageSent.rejected.length) {
+        return res
+          .status(500)
+          .json({ status: "error", message: "message not sent" });
+      }
+      if (messageSent.response.includes(" OK ")) {
+        /* this is probably good */
+
+        cache[email] = new Date().getTime();
+        return res
+          .status(200)
+          .json({ status: "success", message: "message sent" });
+      } else {
+        throw new EmailFailedError("email failed to send");
+      }
+    } catch (e) {
+      return res
+        .status(500)
+        .json({ status: "error", message: "error in POST /sendMail", e });
+    }
+  }
 );
 
-app.post('/blockUser', (req, res) => {
-	const { ip } = req.body;
-	if (!ip) {
-		return res.status(400).json({ status: 'error', message: 'must supply ip to be added to blockList' });
-	}
-	addToBlockList(ip);
-	blockList = loadBlockList();
-	res.json({ blockList });
+app.post("/blockUser", (req, res) => {
+  const { ip } = req.body;
+  if (!ip) {
+    return res.status(400).json({
+      status: "error",
+      message: "must supply ip to be added to blockList",
+    });
+  }
+  addToBlockList(ip);
+  blockList = loadBlockList();
+  res.json({ blockList });
 });
 
 app.listen(3000, () => {
-	console.log('listening on port 3000');
+  console.log("listening on port 3000");
 });
--- src/utils.ts
+++ src/utils.ts
@@ -1,55 +1,68 @@
-import { Request, Response, NextFunction } from 'express';
-import { writeFileSync, readFileSync, appendFileSync } from 'fs';
-import path from 'path';
+import { Request, Response, NextFunction } from "express";
+import { writeFileSync, readFileSync, appendFileSync } from "fs";
+import path from "path";
 
-export function allowedToEmail(email: string, cache: MyCache, blockList: BlockList = []): boolean {
-	if (blockList.includes(email)) {
-		return false;
-	} else {
-		if (!cache[email]) return true;
-		else if (new Date().getTime() - cache[email] > 3.6e6) {
-			delete cache[email];
-			return true;
-		} else return false;
-	}
+export function allowedToEmail(
+  email: string,
+  cache: MyCache,
+  blockList: BlockList = []
+): boolean {
+  if (blockList.includes(email)) {
+    return false;
+  } else {
+    if (!cache[email]) return true;
+    else if (new Date().getTime() - cache[email] > 3.6e6) {
+      delete cache[email];
+      return true;
+    } else return false;
+  }
 }
 
 export function ipInBlockList(ip: BlockedIP, blockList: BlockList) {
-	return blockList.includes(ip);
+  return blockList.includes(ip);
 }
 
-export function rejectInBlockList(req: Request, res: Response, next: NextFunction, blockList: BlockList) {
-	if (ipInBlockList(req.ip, blockList)) {
-		return res.status(429).json({
-			status: 'error',
-			message: `user with IP addr: ${req.ip} is unable to use this feature`,
-		});
-	} else {
-		next();
-	}
+export function rejectInBlockList(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+  blockList: BlockList
+) {
+  if (ipInBlockList(req.ip, blockList)) {
+    return res.status(429).json({
+      status: "error",
+      message: `user with IP addr: ${req.ip} is unable to use this feature`,
+    });
+  } else {
+    next();
+  }
 }
 
 export function loadBlockList() {
-	let blocklist: BlockList;
-	try {
-		blocklist = readFileSync(path.join(__dirname, './blocklist.txt'), 'utf-8')
-			.split('\n')
-			.filter((ip) => ip);
-		// return blocklist;
-	} catch (err) {
-		writeFileSync(path.join(__dirname, './blocklist.txt'), '', { encoding: 'utf-8' });
-		blocklist = readFileSync(path.join(__dirname, './blocklist.txt'), 'utf-8')
-			.split('\n')
-			.filter((ip) => ip);
-	}
-	return blocklist;
+  let blocklist: BlockList;
+  try {
+    blocklist = readFileSync(path.join(__dirname, "./blocklist.txt"), "utf-8")
+      .split("\n")
+      .filter((ip) => ip);
+    // return blocklist;
+  } catch (err) {
+    writeFileSync(path.join(__dirname, "./blocklist.txt"), "", {
+      encoding: "utf-8",
+    });
+    blocklist = readFileSync(path.join(__dirname, "./blocklist.txt"), "utf-8")
+      .split("\n")
+      .filter((ip) => ip);
+  }
+  return blocklist;
 }
 
 export function addToBlockList(ip: BlockedIP) {
-	try {
-		appendFileSync(path.join(__dirname, './blocklist.txt'), '\n' + ip);
-		return true;
-	} catch (err) {
-		return false;
-	}
+  try {
+    appendFileSync(path.join(__dirname, "./blocklist.txt"), "\n" + ip);
+    return true;
+  } catch (err) {
+    return false;
+  }
 }
+
+export class EmailFailedError extends Error {}
